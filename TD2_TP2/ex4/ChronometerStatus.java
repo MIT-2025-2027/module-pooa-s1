8b607e90c0a7b745cb125b5f008dad3c96fe7f15
kfifel <fifel.khalid99@gmail.com>
2025-12-19 00:53:30 +0100

    Revert "Add TD2_TP2 solution"
    This reverts commit 74507497e85514da66582903998a9893ecdc068d.

--- TD2_TP2/ex4/ChronometerStatus.java
+++
@@ -1,6 +0,0 @@
-package TD2_TP2.ex4;
-
-public enum ChronometerStatus {
-
-	INIT, RUNNING, STOPPED
-}
--- TD2_TP2/ex4/ChronometreInterface.java
+++
@@ -1,9 +0,0 @@
-package TD2_TP2.ex4;
-
-public interface ChronometreInterface {
-	
-	public void start();
-	public void stop();
-	public long getTime();
-	public void raz();
-}
